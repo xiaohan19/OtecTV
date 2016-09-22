08c29de6ce706eec0b97fc3050d6f291a8475ffe
linhao <xiaohan-lh@163.com>
2016-09-22 10:54:23 +0800

    注释编译不通过的代码

--- app/src/main/java/com/chinaotec/tv/otectv/MainActivity.java
+++ app/src/main/java/com/chinaotec/tv/otectv/MainActivity.java
@@ -3,6 +3,7 @@ package com.chinaotec.tv.otectv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -16,21 +17,21 @@ public class MainActivity extends AppCompatActivity {
 //        init();
     }
 
-//    private void init() {
-//        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.main_rl);
-//        for (int i = 0; i < relativeLayout.getChildCount(); i++) {
-//            relativeLayout.getChildAt(i).setFocusable(true);
-//            relativeLayout.getChildAt(i).setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//                @Override
-//                public void onFocusChange(View v, boolean hasFocus) {
-//                    if (hasFocus) {
-//                        v.setBackgroundResource(R.drawable.main_select_true);
-//                    }else {
-//                        v.setBackgroundResource(R.color.transparent);
-//                    }
-//                }
-//            });
-//        }
-//    }
+    private void init() {
+        FrameLayout relativeLayout = (FrameLayout) findViewById(R.id.main_fl);
+        for (int i = 0; i < relativeLayout.getChildCount(); i++) {
+            relativeLayout.getChildAt(i).setFocusable(true);
+            relativeLayout.getChildAt(i).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        v.setBackgroundResource(R.drawable.main_select_true);
+                    }else {
+                        v.setBackgroundResource(R.color.transparent);
+                    }
+                }
+            });
+        }
+    }
 
 }
